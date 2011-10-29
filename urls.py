1649e80baac2bbecd47e69db078a51153072579f
sam <sam.sandberg+hehe@gmail.com>
2011-10-29 14:44:21 +0200

    pushing a placeholder html file for now

--- urls.py
+++ urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+	('', direct_to_template, {'template': 'placeholder.html'}),
     # Examples:
     # url(r'^$', 'novembeard.views.home', name='home'),
     # url(r'^novembeard/', include('novembeard.foo.urls')),
