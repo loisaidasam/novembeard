7aadbff58cd72d0ebdcdf8148d44a75716095684
sam <sam.sandberg@zemanta.com>
2011-10-30 04:55:01 +0100

    moved some things out of settings.py by default

--- settings.py
+++ settings.py
@@ -9,17 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': 'novembeard',					  # Or path to database file if using sqlite3.
-		'USER': 'novembeard',					  # Not used with sqlite3.
-		'PASSWORD': 'OoTiege1PheKahm',				  # Not used with sqlite3.
-		'HOST': '',					  # Set to empty string for localhost. Not used with sqlite3.
-		'PORT': '',					  # Set to empty string for default. Not used with sqlite3.
-	}
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -106,7 +95,6 @@ TEMPLATE_DIRS = (
 	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
-	'/home/sam/novembeard/templates',
 )
 
 INSTALLED_APPS = (
